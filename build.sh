190ef4e3c5af1b842eaa5ca6473d2b29101d4717
Robin Luo <robin_luopc@163.com>
2025-06-10 00:05:24 +0800

    [2025.06.09] add maven config

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ if [ "$BUILD_TYPE" = "release" ]; then
   mvn -B clean release:prepare-with-pom release:perform -DuseReleaseProfile=false -Dmaven.javadoc.skip=true  -Puat
 else
   echo "Building Snapshot Version"
-  mvn -B clean deploy -Dmaven.javadoc.skip=true
+  mvnd -B clean deploy -Dmaven.javadoc.skip=true
   VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec)
   echo "current version is $VERSION"
   echo "Local command: java -jar -Xms128m -Xmx512m -Dspring.profiles.active=dev -Denv=dev {project}/target/{project-jar}.jar"
